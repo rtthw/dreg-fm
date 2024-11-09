e7f6f65017e52deb0884a175f374668944e4bf62
rtthw <172864013+rtthw@users.noreply.github.com>
2024-11-09 07:19:08 -0500

    Setup dreg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+//! Dreg-based File Manager
+
+
+
+use dreg::prelude::*;
+
+
+
+fn main() -> Result<()> {
+    CrosstermPlatform::new()?
+        .run(FileManager {
+            should_exit: false,
+        })
 }
+
+
+
+pub struct FileManager {
+    should_exit: bool,
+}
+
+impl Program for FileManager {
+    fn update(&mut self, _frame: Frame) {
+    }
+
+    fn on_input(&mut self, input: Input) {
+        match input {
+            Input::KeyDown(Scancode::Q) => {
+                self.should_exit = true;
+            }
+            _ => {}
+        }
+    }
+
+    fn on_platform_request(&mut self, _request: &str) -> Option<&str> { None }
+
+    fn should_exit(&self) -> bool { self.should_exit }
+}
+
