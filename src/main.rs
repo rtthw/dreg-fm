a2df222ec35c80f6b04f3ff39897a00d98e4156c
rtthw <172864013+rtthw@users.noreply.github.com>
2024-11-09 09:02:40 -0500

    Allow toggling of hidden files

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() -> Result<()> {
         .run(FileManager {
             dir: DirContent::new(std::env::current_dir()?)?,
             should_exit: false,
+            show_hidden_files: true,
         })
 }
 
@@ -42,6 +43,7 @@ struct Cli {
 pub struct FileManager {
     dir: DirContent,
     should_exit: bool,
+    show_hidden_files: bool,
 }
 
 impl Program for FileManager {
@@ -50,9 +52,9 @@ impl Program for FileManager {
         let (left_area, right_area) = area.hsplit_portion(0.2);
         let (middle_area, right_area) = right_area.hsplit_portion(0.5);
 
-        Block::new(Style::new()).render(left_area, &mut frame.buffer);
+        Block::new(Style::new().dim()).render(left_area, &mut frame.buffer);
         Block::new(Style::new()).render(middle_area, &mut frame.buffer);
-        Block::new(Style::new()).render(right_area, &mut frame.buffer);
+        Block::new(Style::new().dim()).render(right_area, &mut frame.buffer);
 
         self.render_middle(middle_area.inner(1, 1), &mut frame.buffer);
     }
@@ -60,6 +62,7 @@ impl Program for FileManager {
     fn on_input(&mut self, input: Input) {
         match input {
             Input::KeyDown(Scancode::Q) => self.handle_command("exit"),
+            Input::KeyDown(Scancode::H) => self.handle_command("toggle_show_hidden_files"),
             _ => {}
         }
     }
@@ -75,20 +78,43 @@ impl FileManager {
             Command::Exit => {
                 self.should_exit = true;
             }
+            Command::ToggleShowHiddenFiles => {
+                self.show_hidden_files = !self.show_hidden_files;
+            }
         }
+    }
+
+    pub fn iter_dir(&self) -> impl Iterator<Item = &DirEntry> {
+        self.dir.children.iter()
+            .filter(|e| {
+                if !self.show_hidden_files {
+                    if e.file_name().to_str().is_some_and(|s| s.starts_with(".")) {
+                        return false;
+                    }
+                }
+
+                true
+            })
     }
 }
 
 impl FileManager {
     fn render_middle(&mut self, area: Rect, buf: &mut Buffer) {
         // TODO: Scrolling.
-        for (row, entry) in area.rows().into_iter().zip(self.dir.children.iter()) {
+        for (row, entry) in area.rows().into_iter().zip(self.iter_dir()) {
+            let fg = if entry.path().is_dir() {
+                Color::Blue
+            } else if entry.path().is_symlink() {
+                Color::Yellow
+            } else {
+                Color::Gray
+            };
             buf.set_stringn(
                 row.x,
                 row.y,
                 entry.file_name().to_string_lossy(),
                 row.width as usize,
-                Style::new().dim().fg(Color::Green),
+                Style::new().dim().fg(fg),
             );
         }
     }
@@ -99,12 +125,14 @@ impl FileManager {
 #[derive(Clone, Copy)]
 pub enum Command {
     Exit,
+    ToggleShowHiddenFiles,
 }
 
 impl From<&'static str> for Command {
     fn from(value: &'static str) -> Self {
         match value {
             "exit" => Self::Exit,
+            "toggle_show_hidden_files" => Self::ToggleShowHiddenFiles,
             c => unreachable!("invalid command initializer: {c}"),
         }
     }
@@ -120,10 +148,19 @@ pub struct DirContent {
 impl DirContent {
     pub fn new(path: impl Into<PathBuf>) -> Result<Self> {
         let path = path.into();
-        let children = std::fs::read_dir(&path)?
+        let mut children: Vec<DirEntry> = std::fs::read_dir(&path)?
             .filter_map(|e| e.ok())
             .collect();
-
+        children.sort_by(|a, b| {
+            if a.path().is_dir() == b.path().is_dir() {
+                a.file_name().cmp(&b.file_name())
+            } else if a.path().is_dir() {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
+        });
+        
         Ok(Self {
             path,
             children,
